afb29a83136afc9f411660355006ae50a974d27b
simeo russo <simeo94@hotmail.com>
2020-10-21 16:24:34 +0100

    update to search componentvia axios get request

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,40 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const Search = () => {
 
   const [term, setTerm] = useState("");
+  const [results, setresults] = useState([]);
 
   useEffect(() => {
-    console.log(term)
+    const search = async () => {
+      const {data} = await axios.get(`https://en.wikipedia.org/w/api.php`, {
+        params:{
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term
+        },
+      });
+      setresults(data.query.search)
+    };
+    if (term) {search()}
   },[term]);
+
+   const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span> //note security
+        </div>
+        <div className="right floated content">
+          <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`}>Go</a>
+        </div>
+      </div>
+    );
+  });
 
   return(
     <div>
@@ -15,6 +43,10 @@ const Search = () => {
           <label>Enter Search Term</label>
           <input className="input" type="text" vlaue={term} onChange={e => setTerm(e.target.value)}/>
         </div>
+      </div>
+
+      <div className="ui celled list">
+        {renderedResults}
       </div>
     </div>
    );
