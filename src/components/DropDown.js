b6dd1ea63e8e39d90c6750c0eb8c4507c034d5e5
simeo russo <simeo94@hotmail.com>
2020-10-23 12:07:33 +0100

    added event listner to close dropdown

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
 const DropDown = ({options, selected, onSelectedChange}) =>{
+
+  const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  // close dropdown when click outide of it
+  useEffect(() => {
+    document.body.addEventListener('click', (e) => {
+      if (ref.current.contains(e.target)){
+        return;
+      }
+      setOpen(false)
+    });
+  }, []);
 
   const renderdOptions = options.map((option) => {
 
     if (option.value === selected.value){
       return null;
     }
-
     return(
         <div className="item" key={option.value} onClick={() => onSelectedChange(option)}>
           {option.label}
@@ -16,13 +28,13 @@ const DropDown = ({options, selected, onSelectedChange}) =>{
   });
 
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
-        <div className="ui selection dropdown visible active">
+        <div onClick={() => setOpen(!open)}className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className="menu visible transition">{renderdOptions}</div>
+          <div className={`menu ${open ?'visible transition' : ''}`}>{renderdOptions}</div>
         </div>
       </div>
     </div>
